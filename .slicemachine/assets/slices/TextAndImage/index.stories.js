a84a5f595775e490218c3093caafc9b027a1a7a3
Fernando Cardozo <fhpcardozo@gmail.com>
2023-02-15 11:35:00 -0300

    update: update PrismicRichText component call to better handle links

--- .slicemachine/assets/slices/TextAndImage/index.stories.js
+++ .slicemachine/assets/slices/TextAndImage/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"image":{"dimensions":{"width":3199,"height":3199},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1600804931749-2da4ce26c869"},"text_content":[{"type":"paragraph","text":"Cupidatat magna nisi aute in veniam ut sint eu ex.","spans":[]}],"external_link":{"link_type":"Web","url":"http://google.com"}},"id":"_Default","slice_type":"text_and_image"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"image":{"dimensions":{"width":3935,"height":5894},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1587905069134-008460d7a636"},"text_content":[{"type":"paragraph","text":"Ex ullamco commodo cillum est. Commodo et in aute dolor proident dolor tempor aute eu veniam non eu nulla exercitation.","spans":[]}]},"id":"_Default","slice_type":"text_and_image"}} />
 _Default.storyName = ''
--- .slicemachine/assets/slices/TextOnly/index.stories.js
+++ .slicemachine/assets/slices/TextOnly/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"text_content":[{"type":"paragraph","text":"Culpa cillum esse exercitation non non ex.","spans":[]}],"external_link":{"link_type":"Web","url":"https://prismic.io"}},"id":"_Default","slice_type":"text_only"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"text_content":[{"type":"paragraph","text":"Culpa cillum esse exercitation non non ex.","spans":[]}]},"id":"_Default","slice_type":"text_only"}} />
 _Default.storyName = ''
