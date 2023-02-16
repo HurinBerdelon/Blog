b4cfb07b89e2885db2b859a1e8923fe8b53b8798
Fernando Cardozo <fhpcardozo@gmail.com>
2023-02-16 18:33:16 -0300

    feat: add preview apis

---
+++ src/pages/api/preview.tsx
@@ -0,0 +1,9 @@
+import { redirectToPreviewURL, setPreviewData } from "@prismicio/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "prismicio";
+
+export default async function Preview(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+    const client = createClient({ req })
+    await setPreviewData({ req, res })
+    await redirectToPreviewURL({ req, res, client })
+}
