e3aeae33a39cdaf15f0fa9a463f7ee37f630701c
Fernando Cardozo <fhpcardozo@gmail.com>
2023-02-24 14:06:19 -0300

    fix: fix height of postCard

--- src/components/Posts/PostCard.tsx
+++ src/components/Posts/PostCard.tsx
@@ -20,20 +20,20 @@ export function PostCard({ post }: PostCardProps): JSX.Element {
 
     return (
         <motion.li
-            className="border-[1px] border-backgroundDark dark:border-textLight w-[80vw] self-center rounded max-w-xs hover:border-greenBrandDark hover:text-greenBrandDark dark:hover:text-grayBrand dark:hover:border-grayBrand"
+            className="border-[1px] h-[460px] border-backgroundDark dark:border-textLight w-[80vw] self-center rounded max-w-xs hover:border-greenBrandDark hover:text-greenBrandDark dark:hover:text-grayBrand dark:hover:border-grayBrand"
             whileHover={{
                 scale: 1.025
             }}
         >
-            <Link href={`/post/${post.uid}`} className="flex flex-col gap-2">
+            <Link href={`/post/${post.uid}`} className="flex flex-col gap-2 flex-1 h-full justify-between">
                 <h3 className="self-center text-xl text-center px-2 font-semibold py-2">{post.data.title_of_the_post}</h3>
-                <div className="w-full h-[240px] flex items-center">
+                <div className="w-full flex items-center">
                     <Image
                         width={post.data.banner.dimensions?.width}
                         height={post.data.banner.dimensions?.height}
                         src={post.data.banner.url as string}
                         alt={post.data.banner.alt as string}
-                        className="object-cover w-full"
+                        className="object-cover w-full flex-1"
                     />
                 </div>
                 <div className="px-3 flex gap-3 italic font-medium capitalize text-sm">
