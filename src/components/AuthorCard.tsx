69fa10f35eb599e215bbaa468f4044e2bf80d349
Fernando Cardozo <fhpcardozo@gmail.com>
2023-02-24 13:59:05 -0300

    feat: switch order of image and text in imageAndText slice

--- src/components/AuthorCard.tsx
+++ src/components/AuthorCard.tsx
@@ -14,16 +14,16 @@ export function AuthorCard({ post }: AuthorCardProps): JSX.Element {
     return (
         <div className="flex gap-2 items-center">
             <div className="w-10 rounded-full">
-                <Image
+                {/* <Image
                     width={40}
                     height={40}
                     src={author.data.authorprofileimage.url}
                     alt={author.data.authorprofileimage.alt}
                     className="rounded-full object-cover"
-                />
+                /> */}
             </div>
             <div className="flex flex-col">
-                <span className="font-medium">{author.data.name}</span>
+                {/* <span className="font-medium">{author.data.name}</span> */}
                 <span className="italic text-sm">{formatDate(post.first_publication_date)}</span>
             </div>
         </div>
