74197f27ba70781eb74c335b06445d1121ee77ae
Fernando Cardozo <fhpcardozo@gmail.com>
2023-02-27 17:16:37 -0300

    fix: add id to analytic return, fixing build bug

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -3,9 +3,9 @@ import Script from "next/script"
 export function GoogleAnalytic(): JSX.Element {
     return (
         <>
-            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+            <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} id="analyticLoading" />
 
-            <Script strategy="lazyOnload">
+            <Script strategy="lazyOnload" id="analytics">
                 {`
 					window.dataLayer = window.dataLayer || [];
 					function gtag(){dataLayer.push(arguments);}
