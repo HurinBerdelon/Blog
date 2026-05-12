17424b4ac737295acfa62bb373c3d312c5653150
Fernando Cardozo <fhpcardozo@gmail.com>
2026-05-12 12:44:25 -0300

    feat: update validation on read from undefined

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,5 +1,5 @@
-import { motion } from 'framer-motion'
-import { useTranslation } from 'next-i18next/pages'
+import { motion } from "framer-motion";
+import { useTranslation } from "next-i18next/pages";
 import {
     FacebookShareButton,
     FacebookIcon,
@@ -8,56 +8,52 @@ import {
     LinkedinShareButton,
     LinkedinIcon,
     WhatsappShareButton,
-    WhatsappIcon
-} from 'react-share'
+    WhatsappIcon,
+} from "react-share";
 
 interface ShareProps {
-    postLink: string
-    title: string
+    postLink: string;
+    title: string;
 }
 
 export function Share({ postLink, title }: ShareProps): JSX.Element {
-    console.log({ postLink })
-
-    const { t } = useTranslation()
+    const { t } = useTranslation();
 
     return (
         <section>
-            <span className='font-semibold text-sm mb-2 block'>{t('common:shareThisPost')}</span>
-            <ul className='flex gap-2'>
-                <motion.li
-                    whileHover={{ scale: 1.1 }}
-                >
+            <span className="block mb-2 text-sm font-semibold">
+                {t("common:shareThisPost")}
+            </span>
+            <ul className="flex gap-2">
+                <motion.li whileHover={{ scale: 1.1 }}>
                     <FacebookShareButton url={postLink} quote={title}>
-                        <FacebookIcon className='w-8 h-8 rounded' />
-                        <span className='sr-only'>{`${t('common:shareOn')} Facebook`}</span>
+                        <FacebookIcon className="w-8 h-8 rounded" />
+                        <span className="sr-only">{`${t("common:shareOn")} Facebook`}</span>
                     </FacebookShareButton>
                 </motion.li>
-                <motion.li
-                    whileHover={{ scale: 1.1 }}
-                >
+                <motion.li whileHover={{ scale: 1.1 }}>
                     <TwitterShareButton url={postLink} title={title}>
-                        <TwitterIcon className='w-8 h-8 rounded' />
-                        <span className='sr-only'>{`${t('common:shareOn')} Twitter`}</span>
+                        <TwitterIcon className="w-8 h-8 rounded" />
+                        <span className="sr-only">{`${t("common:shareOn")} Twitter`}</span>
                     </TwitterShareButton>
                 </motion.li>
-                <motion.li
-                    whileHover={{ scale: 1.1 }}
-                >
-                    <LinkedinShareButton url={postLink} title={title} source={"blog.hurinberdelon.com"}>
-                        <LinkedinIcon className='w-8 h-8 rounded' />
-                        <span className='sr-only'>{`${t('common:shareOn')} LinkedIn`}</span>
+                <motion.li whileHover={{ scale: 1.1 }}>
+                    <LinkedinShareButton
+                        url={postLink}
+                        title={title}
+                        source={"blog.hurinberdelon.com"}
+                    >
+                        <LinkedinIcon className="w-8 h-8 rounded" />
+                        <span className="sr-only">{`${t("common:shareOn")} LinkedIn`}</span>
                     </LinkedinShareButton>
                 </motion.li>
-                <motion.li
-                    whileHover={{ scale: 1.1 }}
-                >
+                <motion.li whileHover={{ scale: 1.1 }}>
                     <WhatsappShareButton url={postLink} title={title}>
-                        <WhatsappIcon className='w-8 h-8 rounded' />
-                        <span className='sr-only'>{`${t('common:shareOn')} WhatsApp`}</span>
+                        <WhatsappIcon className="w-8 h-8 rounded" />
+                        <span className="sr-only">{`${t("common:shareOn")} WhatsApp`}</span>
                     </WhatsappShareButton>
                 </motion.li>
             </ul>
         </section>
-    )
+    );
 }
